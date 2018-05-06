4fa5bc6571b47f645ac5cfcfecfd4adea5622a44
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-05-06 16:12:15 -0300

    Printer refatored

--- avlTreePrinter.h
+++ avlTreePrinter.h
@@ -5,23 +5,36 @@
  *  ============================
  */
 
-void padding ( char ch, int n ){
-  for (int i = 0; i < n; i++ )
-    putchar ( ch );
+void padding(int n){
+  for(int i = 0; i < n; i++ )
+    printf("\t");
 }
-void printStructure ( struct avl_node *root, int level ){
+void print_node(avl_node *node, int level) {
+  int height = node->r_height > node->l_height ? node->r_height : node->l_height;
 
-  if ( root == NULL ) {
-    padding ( '\t', level+1 );
-    puts ( "~" );
+  char str[32];
+  sprintf(str, "(\x1b[1m%d\x1b[0m|h:%d)", node->data, height);
+  printf("%s", str);
+
+  int dash = 80 - (level * 8); // 80 is the total available space, level * 8 is the tab size
+
+  // For each caracter on string remove 1 from dash
+  // Starts in 14 because str has 14 non-print characters (like \x1b[1m for coloring)
+  for(int i=14; str[i] != '\0'; i++) dash--;
+
+  for(int i=0; i < dash; i++) printf("-");
+  puts("");
+}
+void print_tree (avl_node *root, int level){
+
+  if (!root) {
+    padding (level+1);
+    puts("~");
   }
   else {
-    printStructure ( root->right, level + 1 );
-    padding ( '\t', level+1 );
-
-    int height = root->r_height > root->l_height ? root->r_height : root->l_height;
-    printf ( "(\x1b[1m%d\x1b[0m|h:%d)--------------------------------------------\n", root->data, height );
-    // printf("(e:%d|%d|d:%d)\n", root->l_height, root->data, root->r_height);
-    printStructure ( root->left, level + 1 );
+    print_tree (root->right, level + 1);
+    padding (level + 1);
+    print_node(root, level + 1);
+    print_tree(root->left, level + 1);
   }
 }
